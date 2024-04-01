62db888947b1904febbfea919a336e9a21f2cd0e
Aditya Devasar <90569739+RickyDevasar@users.noreply.github.com>
2024-04-01 16:52:45 +0530

    Update db.js

--- Backend/Config/db.js
+++ Backend/Config/db.js
@@ -6,7 +6,7 @@ const connectDB=async()=>{
    try{
     const conn=await mongoose.connect(process.env.MONGO_URI,{
         useNewUrlParser:true,
-        useUnifiedTopology:true
+        
         
         
     })
