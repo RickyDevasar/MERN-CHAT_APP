e0b6e91d100318354a6e742cc2e2a9a36ea2c9f2
Aditya Devasar <90569739+RickyDevasar@users.noreply.github.com>
2024-04-01 22:00:13 +0530

    Update SingleChat.js

--- frontend/src/Components/SingleChat.js
+++ frontend/src/Components/SingleChat.js
@@ -16,7 +16,7 @@ import { TypeAnimation } from 'react-type-animation';
 import io from "socket.io-client";
 import UpdateGroupChatModal from "./miscellaneous/UpdateGroupChatModal";
 import { ChatState } from "../Context/ChatProvider";
- const ENDPOINT = "http://localhost:5000"; // "https://talk-a-tive.herokuapp.com"; -> After deployment
+ const ENDPOINT = "https://mern-chat-app-k342.onrender.com"; // "https://talk-a-tive.herokuapp.com"; -> After deployment
  var socket, selectedChatCompare;
 
 const SingleChat = ({ fetchAgain, setFetchAgain }) => {
